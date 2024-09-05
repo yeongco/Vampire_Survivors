b5126be589abc59643b10147a0132374542cfca9
meo-young <meoyoung000@naver.com>
2024-09-06 02:50:46 +0900

    적인식 구현
    Navmesh로 구현하면 map이 재생성 될 때마다 경로를 찾는데에 버그가 발생해서 navmesh가 아닌 rigidbody로 추적하는 알고리즘 수정

---
+++ Assets/Scripts/Meoyoung/Enemy/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("적이 이동할 속도")]
+    [SerializeField] float speed;
+
+    [Tooltip("적이 추적할 플레이어")]
+    [SerializeField] Rigidbody target;
+
+    private bool isLive;
+    private Rigidbody rigid;
+
+    private void Awake()
+    {
+        isLive = true;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isLive)
+        {
+            Vector3 dirVec = target.position - rigid.position;
+            Vector3 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+            rigid.MovePosition(rigid.position + nextVec);
+            rigid.velocity = Vector3.zero;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+}
