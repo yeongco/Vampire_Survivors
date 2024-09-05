95074dcba20b10d041070c8e39634f8309c6c611
meo-young <meoyoung000@naver.com>
2024-09-05 13:54:01 +0900

    무한 맵 구현

--- Assets/Scripts/Meoyoung/Map/Reposition.cs
+++ Assets/Scripts/Meoyoung/Map/Reposition.cs
@@ -7,11 +7,13 @@ public class Reposition : MonoBehaviour
     {
         if (other.CompareTag("Area")) //플레이어가 x축으로 멀어진건지 y축으로 멀어진건지 체크
         {
+            Debug.Log("Area 충돌");
+
             Vector3 playerPos = GameManager.instance.player.transform.position;
             Vector3 myPos = transform.position;
 
             float diffX = Mathf.Abs(playerPos.x - myPos.x);
-            float diffY = Mathf.Abs(playerPos.y - myPos.y);
+            float diffY = Mathf.Abs(playerPos.z - myPos.z);
 
             Vector3 playerDir = GameManager.instance.player.movement;
             float dirX = 0;
@@ -37,13 +39,15 @@ public class Reposition : MonoBehaviour
             switch (transform.tag)
             {
                 case "Ground":
+                    Debug.Log("Tag : Ground");
                     if(diffX > diffY)
                     {
-                        transform.Translate(Vector3.right * dirX * 10);
+                        transform.Translate(Vector3.right * dirX * 200);
                     }
                     else if (diffX < diffY)
                     {
-                        transform.Translate(Vector3.up * dirY * 10);
+                        Vector3 upVector = new(0, 0, 1);
+                        transform.Translate(upVector * dirY * 200);
                     }
                     break;
                 case "Enemy":
