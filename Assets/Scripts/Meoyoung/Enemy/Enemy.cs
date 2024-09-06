f0f17ee9cda5436ecbde43b120def17c71aceabd
meo-young <meoyoung000@naver.com>
2024-09-06 12:04:11 +0900

    Enemy 회전 수정

--- Assets/Scripts/Meoyoung/Enemy/Enemy.cs
+++ Assets/Scripts/Meoyoung/Enemy/Enemy.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [Tooltip("적이 이동할 속도")]
     [SerializeField] float speed;
+
+    [Tooltip("적의 회전 속도")]
+    [SerializeField] float rotationSpeed = 1f;
 
     [Tooltip("적이 추적할 플레이어")]
     [SerializeField] Rigidbody target;
@@ -24,9 +24,16 @@ public class Enemy : MonoBehaviour
     {
         if (isLive)
         {
+            //플레이어 방향으로 천천히 회전
+            Vector3 direction = (target.position - transform.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            targetRotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+
+            //플레이어 방향으로 이동
             Vector3 dirVec = target.position - rigid.position;
+            dirVec.y = 0f;
             Vector3 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
-            transform.LookAt(target.transform);
             rigid.MovePosition(rigid.position + nextVec);
             rigid.velocity = Vector3.zero;
         }
